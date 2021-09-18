f6707809665ab44c288147c7fb1a8f0fb78c66ec
Ana Maria <annasantib@gmail.com>
2021-09-18 14:10:36 -0300

    terminado

--- javascript.js
+++ javascript.js
@@ -41,29 +41,52 @@ let products = [
         precio: 1000,
         stock: 5
     }
-]
-const respaldoProductos = [...products]
-let carrito = [];
-let seccionProductos = document.getElementById("products")
-let restaurarProductosBoton = document.getElementById("restaurarProductos")
-let cacheDeEventos = []
+], 
+carrito = [],
+cacheDeEventos = [],
+logeado = false
 
+const respaldoProductos = [...products], 
+usuario = "materjs",
+password = "quiero120puntospls",
+seccionProductos = document.getElementById("products"),
+restaurarProductosBoton = document.getElementById("restaurarProductos"),
+loginBoton = document.getElementById("login"),
+loginError = document.getElementById("login-error"),
+loginSection = document.getElementById("loginSection"),
+botonCompra = document.getElementById("botonCompra"),
+detalleCarritoCompra = document.getElementById("detail-carro"),
+totalCarritoCompra = document.getElementById("total-carro"),
+exito = document.getElementById("exito"),
+costoEnvio = 1500,
+costoEnvioPorProducto = 350
+
+/**
+ * carga los productos y agrega al carrito de compra
+ */
 const cargarProductos = () => {
-    console.log(" cargando productos", seccionProductos)
+    // console.log(" cargando productos", seccionProductos)
 
     //obtengo productos desde localstorage
     let storageDeProductos = localStorage.getItem('productos')
+    let storageDeCarritoCompra = localStorage.getItem('carrito')
 
     //realizo try catch para evitar errores en el parse
     try {
         storageDeProductos = JSON.parse(storageDeProductos)
+        storageDeCarritoCompra = JSON.parse(storageDeCarritoCompra)
     } catch (error) {
-        console.log('Ocurrio un horror al cargar los items')
+        //Esto en realidad no es un error, resulta que para reiniciar el localstorage, se deja como un string vacio, por tal, no puede convertirlo a objeto
+        // console.log('Ocurrio un horror al cargar los items')
     }
     
     //si el storage de productos contiene algo, remplazo el array de productos
     if(storageDeProductos && storageDeProductos.length > 0){
         products = [...storageDeProductos]
+    }
+
+    if(storageDeCarritoCompra && storageDeCarritoCompra.length > 0){
+        carrito = [...storageDeCarritoCompra]
     }
 
     //cargar los productos dinamicamente
@@ -91,8 +114,11 @@ const cargarProductos = () => {
         p2.innerText = `${producto.stock} unidades disponibles`
         section.appendChild(p2)
     
-        const anadirCompra = (evento) => {
+        const anadirProducto = (evento) => {
             evento.preventDefault()
+
+            //eliminamos mensaje de exito si esta con display block
+            exito.classList.remove('mostrar')
 
             let idProducto = evento.target.getAttribute("data-idproducto")
 
@@ -108,7 +134,32 @@ const cargarProductos = () => {
                     return false
 
                 //agregar al carrito de compra
-                carrito.push(productoEncontrado)
+                if(carrito.find(item => item.id === idProducto) !== undefined){
+                    //si ya existe sumamos el producto al total
+                    carrito = carrito.map(item => {
+                        if(item.id === idProducto){
+                            return  {
+                                ...item,
+                                total: item.precio + item.total,
+                                stock: item.stock - 1,//aprovechamos de mapear el stock por si a caso
+                                unidades: item.unidades + 1
+                            }
+                        } else {
+                            return item
+                        }
+                    })
+                } else {
+                    //le agregamos total para luego sumarlo si existe mas de uno
+                    productoEncontrado = {
+                        ...productoEncontrado,
+                        total: productoEncontrado.precio,
+                        stock:  productoEncontrado.stock - 1,//aprovechamos de mapear el stock por si a caso
+                        unidades: 1
+                    }
+                    
+                    carrito.push(productoEncontrado)
+                }
+                
 
                 //mapeamos objeto productos para cambiar su stock
                 products = [...products.map(producto => {
@@ -129,12 +180,13 @@ const cargarProductos = () => {
                 }
 
                 localStorage.setItem('productos', JSON.stringify(products))
+                localStorage.setItem('carrito', JSON.stringify(carrito))
                 //volver a pintar
                 cargarProductos()
                 
             }
 
-            console.log('carrito de compra', carrito)
+            // console.log('carrito de compra', carrito)
 
         }
 
@@ -142,20 +194,28 @@ const cargarProductos = () => {
         button.classList.add('btn-add')
         button.innerText = 'Anadir a compra'
         button.dataset.idproducto = producto.id
-        button.addEventListener('click', anadirCompra, false)
+        button.addEventListener('click', anadirProducto, false)
         cacheDeEventos.push(button)
         section.appendChild(button)
     
         seccionProductos.appendChild(section)
+
+        //actualizamos carrito de compra
+        actualizarCarritoDeCompra()
     })
 
 }
 
+/**
+ * Restaura los productos y carrito de compra
+ * @param {*} evento 
+ */
 const resturarProductos = (evento) => {
     evento.preventDefault()
 
     //reemplazando con una copia respaldada los productos
     products = [...respaldoProductos]
+    carrito = []
 
     //liberamos todos el evento por que vamos a volver a pintar los productos
     if(cacheDeEventos){
@@ -165,6 +225,7 @@ const resturarProductos = (evento) => {
 
     //dejamos el localstorage vacio
     localStorage.setItem('productos', '')
+    localStorage.setItem('carrito', '')
 
     //volvemos a pintar la lista
     cargarProductos()
@@ -173,8 +234,130 @@ const resturarProductos = (evento) => {
 
 restaurarProductosBoton.addEventListener('click', resturarProductos)
 
-function carritoDeCompra() {
+/**
+ * actualiza y pinta carrito de compra
+ */
+const actualizarCarritoDeCompra = () => {
+
+    totalCarritoCompra.innerHTML = ''
+    detalleCarritoCompra.innerHTML = ''
+
+    //<h3 class="tit-carro">Carro de Compras</h3>
+    let h3 = document.createElement('h3')
+    h3.innerText = 'Carro de Compras'
+    h3.classList.add('tit-carro')
+
+    detalleCarritoCompra.appendChild(h3)
+    
+    carrito.forEach(producto => {
+
+        let section = document.createElement('section')
+        section.classList.add('detail-carro')
+
+        let imagen = document.createElement('img')
+        imagen.src = producto.imagen
+        imagen.classList.add('img-detail')
+
+        let ul = document.createElement('ul')
+        ul.classList.add('detail-lista')
+        let liNombreProducto = document.createElement('li')
+        liNombreProducto.innerText = producto.nombre
+        let liPrecio = document.createElement('li')
+        liPrecio.innerText = `$${producto.precio}`
+        let liUnidades = document.createElement('li')
+        liUnidades.innerText = producto.unidades > 1 ? `${producto.unidades} unidades` : `${producto.unidades} unidad`
+        let liTotal = document.createElement('li')
+        liTotal.innerText = `total $${producto.total}`
+
+        section.appendChild(imagen)
+        ul.appendChild(liNombreProducto)
+        ul.appendChild(liPrecio)
+        ul.appendChild(liUnidades)
+        ul.appendChild(liTotal)
+        section.appendChild(ul)
+        detalleCarritoCompra.appendChild(section)
+
+    })
+
+    let ul = document.createElement('ul')
+    ul.classList.add('detail-total')
+
+    let totalProductos = carrito.map(x => x.total).reduce((a, b) => a + b, 0)
+    let totalProductosHTML = document.createElement('li')
+    totalProductosHTML.innerText = `Total de productos $${totalProductos}`
+
+    let totalEnvio = (carrito.length * costoEnvioPorProducto) + costoEnvio
+    let totalEnvioHTML = document.createElement('li')
+    totalEnvioHTML.innerText = `Envio $${totalEnvio}`
+
+    let totalHTML = document.createElement('li')
+    totalHTML.innerText = `Total a comprar $${totalEnvio + totalProductos}`
+
+    ul.appendChild(totalProductosHTML)
+    ul.appendChild(totalEnvioHTML)
+    ul.appendChild(totalHTML)
+
+    totalCarritoCompra.appendChild(ul)
 
 }
 
+/**
+ * logea al usuario con usuario y contraseña como constantes
+ * @param {*} event 
+ * @returns 
+ */
+const login = (event) => {
+    event.preventDefault()
+
+    const usuarioInput = document.getElementById("usuario"), passwordInput = document.getElementById("password")
+
+    if(!usuarioInput || usuarioInput.value === '' || usuarioInput.value !== usuario){
+        loginError.classList.add("mostrar")
+        return false
+    }
+
+    if(!passwordInput || passwordInput.value === '' || passwordInput.value !== password){
+        loginError.classList.add("mostrar")
+        return false
+    }
+
+    loginError.classList.remove("mostrar")
+
+    loginSection.innerHTML = ''
+
+    let h1 = document.createElement("h1")
+    h1.innerText = `Bienvenido ${usuario}.`
+    loginSection.appendChild(h1)
+
+    logeado = true
+    botonCompraValidacion()
+
+}
+
+loginBoton.addEventListener('click', login)
+
+/**
+ * boton de compra parte desactivado, cuando se logea cambia a logeado
+ */
+const botonCompraValidacion = () => {
+    botonCompra.disabled = !logeado
+}
+
+/**
+ * muestra mensaje de exito y restaura el carrito y los productos
+ * @param {*} event 
+ */
+const realizarCompra = (event) => {
+    event.preventDefault()
+
+    //mostramos mensaje de exito
+    exito.classList.add('mostrar')
+
+    //restauramos los productos para que vuelva a agregar al carrito
+    resturarProductos(event)
+}
+botonCompra.addEventListener('click', realizarCompra)
+
+botonCompraValidacion()
 cargarProductos()
+actualizarCarritoDeCompra()
